7c0c53d391450b19e855d2295198429f11ff4da7
dil-chhetri <dilchhetri1999@gmail.com>
2024-06-16 22:32:21 +0545

    yey

--- solutions/04_js/components/src/index.js
+++ solutions/04_js/components/src/index.js
@@ -3,30 +3,94 @@ import ReactDOM from 'react-dom'
 import userImage from './images/01.jpg'
 
 
+const welcome = 'Welcome to 30 Days of React'
+const title = 'Getting Started React'
+const subtitle = 'JavaScript Library'
+const author = {firstName: 'Dil', lastName: 'Chhetri'}
+const date = 'Oct 3, 2023'
+
 const Header = () => (
   <header>
   <div className='header-wrapper'>
-    <h1>Welcome to 30 Days Of React</h1>
-    <h2>Getting Started React</h2>
-    <h3>JavaScript Library</h3>
-    <p>Asabeneh Yetayeh</p>
-    <small>Oct 3, 2020</small>
+    <h1>{welcome}</h1>
+    <h2>{title}</h2>
+    <h3>{subtitle}</h3>
+    <p>{author.firstName} {author.lastName}</p>
+    <small>{date}</small>
 
   </div>
 </header>
 )
 
+const numOne = 2
+const numTwo = 5
+
+const result = (
+  <p>
+    {numOne} + { numTwo} = {numOne + numTwo}
+  </p>
+)
+
+
+const yearBorn = 2020
+const currentYear = 2024
+
+const age = currentYear - yearBorn
+
+const personAge = (
+  <p>
+  {' '}
+  {author.firstName} {author.lastName} is {age} years old
+  </p>
+)
+
 const UserCard = () => (
   <div className='user-card'>
       <img src={userImage} alt='asabeneh Image' />
+      <h2>{author.firstName} {author.lastName}</h2>
   </div>
 )
 
+
+
+
 const TechList = () => {
-  
+  const techs = ['html', 'css', 'JavaScript']
+  const techsFormatted = techs.map((techs)=><li key={techs}>{techs}</li>)
+  return techsFormatted
 }
 
+const Main = () => (
+  <main>
+    <div className='main-wrapper'>
+        <p>Prerequite to get started react.js</p>
+        <ul>
+          <TechList />
+        </ul>
+        {result}
+        {personAge}
+        <UserCard />
+    </div>
+  </main>
+)
+
+const copyright = '2020'
+const Footer = () => (
+  <footer>
+    <div className='footer-wrapper'>
+        <p>Copyright &copy;{copyright}</p>
+    </div>
+  </footer>
+)
+
+const App = () => (
+  <div className='app'>
+      <Header />
+      <Main />
+      <Footer />
+  </div>
+)
 
 const rootElement = document.getElementById('root')
 
-ReactDOM.render(<Header />, rootElement)
+ReactDOM.render(<App />, rootElement)
