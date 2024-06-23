28711d26f4dd4cbe8aec8df03d3c4b6c8c224e1c
dil-chhetri <dilchhetri1999@gmail.com>
2024-06-23 21:22:37 +0545

    added

--- solutions/09_js/conditional rendering/src/index.js
+++ solutions/09_js/conditional rendering/src/index.js
@@ -5,31 +5,64 @@ const Button = ({text,onclick}) => (
   <button onClick={onclick}>{text}</button>
 )
 
+const Login = () => (
+  <div>
+    <h3>Please Login</h3>
+  </div>
+)
+
+const Welcome = (props) => (
+  <div>
+    <h1>Welcome to 30 Days of React</h1>
+  </div>
+)
+
+const Message = ({message}) => (
+  <div>
+    <h1>{message}</h1>
+  </div>
+)
 
 class App extends React.Component {
   state = {
-    loggedIn: false
+    loggedIn: false,
+    techs: ['html', 'css', 'js'],
+    message: ''
   }
   handleLogin = () => {
     this.setState({
       loggedIn: !this.state.loggedIn
     })
   }
+
+  greetPeople = () => {
+    let message = "Welcome to 30 Days of React Challenge"
+    this.setState({message})
+  }
   render(){
-    let status
-    let text 
-    if(this.state.loggedIn){
-     status = <h2>You are logged in</h2>
-      text = 'Log Out'
-    }else{
-      status = <h2>You're not logged in</h2>
-      text = "Login"
-    }
+    const {loggedIn, techs} = this.state
+   
+    
+    const status = this.state.loggedIn ? <Welcome /> :  <Login />
+     
+    
     
     return(
       <div className='container-wrapper'>
           {status}
-          <Button text={text} onclick={this.handleLogin} />
+          <Button text={this.state.loggedIn ? 'Logout' : 'Login'} onclick={this.handleLogin} />
+          {
+            techs.length === 3 && (
+              <p>You have all the prerequites courses to get stated with React</p>
+            )
+          }
+          {!loggedIn && (
+            <p>Please login to access more information about 30 Days of React Challenge</p>
+          )
+
+          }
+          <Message message={this.state.message} />
+          <Button text='show message' onclick={this.greetPeople} />
       </div>
     )
   }
